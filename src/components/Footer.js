d656271ba343597d469ac8ed4a5f4920c8ac2197
Rom1v <rom1.verfaillie@gmail.com>
2022-10-03 22:43:32 +0200

    modification footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="socialMedia"></div>
       <p>&copy; 2022 r0m1v</p>
     </div>
   );
