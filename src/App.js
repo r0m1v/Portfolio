806c300408c8e9f57c39a427dac669826914e011
Rom1v <rom1.verfaillie@gmail.com>
2022-10-03 22:21:50 +0200

    import index.scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 import Projects from "./pages/Projects";
 
 const App = () => {
